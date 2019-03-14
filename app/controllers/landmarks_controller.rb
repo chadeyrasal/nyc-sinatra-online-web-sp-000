0f963fcdb2096a1c908e3a0ba19d4850657c60b0
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-03-14 18:08:36 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -27,6 +27,7 @@ class LandmarksController < ApplicationController
     @landmark = Landmark.find(params[:id])
     @landmark.name = params[:landmark][:name]
     @landmark.year_completed = params[:landmark][:year_completed]
+    @landmark.save
   end
 
 end
