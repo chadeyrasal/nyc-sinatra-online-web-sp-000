cb9b435d66e2ddf7ff79fdd113cd369e5e694565
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-03-14 18:08:10 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -25,6 +25,7 @@ class LandmarksController < ApplicationController
 
   patch "/landmarks/:id" do
     @landmark = Landmark.find(params[:id])
+    @landmark.name = params[:landmark][:name]
   end
 
 end
